12dd667d12c37643ac166abeebab9e13d85e9e46
jaylenw <mr2cleanj@live.com>
2017-04-01 15:39:28 +0000

    initial app commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name laikaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the laikaApp
+ */
+angular.module('laikaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name laikaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the laikaApp
+ */
+angular.module('laikaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
