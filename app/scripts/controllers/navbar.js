145f6a161f2c832afbe604ad3a22aaa1426b4f63
Cherie Woo <cheriejawoo@yahoo.com>
2017-04-01 22:58:31 -0700

    made it look a little better..

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -10,4 +10,11 @@
 angular.module('laikaApp')
   .controller('AppCtrl', function ($scope) {
     $scope.currentNavItem = 'page1';
-  });
+  })
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue', {
+      'default': 'A200' // use shade 200 for default, and keep all other shades the same
+    })
+    .accentPalette('red');
+});
